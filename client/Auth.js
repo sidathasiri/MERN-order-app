f72f8fc3b04fffd24841ed3e33379f4c8bca004c
sidathasiri <sidathasiri@gmail.com>
2019-01-21 12:06:22 +0530

    refactored Auth.js

--- client/Auth.js
+++ client/Auth.js
@@ -1,5 +1,4 @@
 var axios = require("axios");
-var isAuthorized = false;
 
 module.exports = {
   login: (email, password) => {
@@ -13,23 +12,18 @@ module.exports = {
           const token = response.data.token;
           if (token) {
             localStorage.authToken = token;
-            isAuthorized = true;
             resolve(isAuthorized);
           } else {
-            isAuthorized = false;
             reject("Wrong username or Password");
           }
         })
         .catch(function(error) {
-          isAuthorized = false;
-          console.log(error);
           reject(error);
         });
     });
   },
 
   logout: () => {
-    isAuthorized = false;
     localStorage.removeItem("authToken");
   },
 
@@ -42,24 +36,20 @@ module.exports = {
         })
         .then(function(response) {
           const { error, token } = response.data;
-          console.log("received");
-          console.log(response.data);
           if (error) {
             reject(error);
             isAuthorized = false;
           } else {
             localStorage.authToken = token;
-            isAuthorized = true;
             resolve(isAuthorized);
           }
         })
         .catch(function(error) {
           reject(error);
-          isAuthorized = false;
         });
     });
   },
   isAuthorized: () => {
-    return isAuthorized || localStorage.authToken;
+    return localStorage.authToken;
   }
 };
